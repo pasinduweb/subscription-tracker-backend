f3256cac2d55309a8495da3ab8080296675c886d
Pasindu Manaranjana <algopasindu@gmail.com>
2025-05-10 18:16:48 +0530

    Implement signIn functionality in auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,8 +51,37 @@ export const signUp = async (req, res, next) => {
     }
 };
 
-// eslint-disable-next-line
-export const signIn = async (req, res, next) => {};
+export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            const error = new Error('Incorrect password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        res.status(200).json({
+            success: true,
+            message: 'User signed in successfully',
+            data: { token, user },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
 // eslint-disable-next-line
 export const signOut = async (req, res, next) => {};
