669ca37981bcac04abe348ad3cd7d8ccb98f742e
Pasindu Manaranjana <algopasindu@gmail.com>
2025-05-10 17:40:11 +0530

    Implement signUp functionality in auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,58 @@
-export const signUp = async (req, res, next) => {};
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js';
+import User from '../models/user.model.js';
 
+export const signUp = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            const error = new Error('User already exists');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await User.create(
+            [
+                {
+                    name,
+                    email,
+                    password: hashedPassword,
+                },
+            ],
+            { session }
+        );
+
+        const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+
+            message: 'User created successfully',
+            data: { token, user: newUsers[0] },
+        });
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        return next(error);
+    }
+};
+
+// eslint-disable-next-line
 export const signIn = async (req, res, next) => {};
 
+// eslint-disable-next-line
 export const signOut = async (req, res, next) => {};
